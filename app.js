22fcc09b04622ae59db6ae1e36027c424a692206
deonopinallae <deonopinallae@yandex.ru>
2025-11-28 13:00:55 +0300

    scroll

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+const menuBtn = document.querySelector(".header__btn-menu");
+const menu = document.querySelector(".header__btn-menu--event");
+const x = document.querySelector(".header__btn-menu--x");
 
-const menuBtn = document.querySelector('.header__btn-menu')
-const menu = document.querySelector('.header__btn-menu--event')
-const x = document.querySelector('.header__btn-menu--x')
+menuBtn.addEventListener("click", function () {
+  menu.classList.add("open");
+});
 
-menuBtn.addEventListener('click', function(){
-    menu.classList.add('open')
+x.addEventListener("click", function () {
+  menu.classList.remove("open");
+});
 
-})
-
-x.addEventListener('click', function(){
-    menu.classList.remove('open')    
-
-})
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+      menu.classList.remove("open");
+    }
+  });
+});
